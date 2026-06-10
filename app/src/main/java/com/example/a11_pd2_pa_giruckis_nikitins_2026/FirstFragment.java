7e53b16f5532ba635ca22084d33b2167fa8d0ba0
Ne1k3 <urapobeda33@gmai.com>
2026-06-10 14:39:02 +0300

    fix problem

--- app/src/main/java/com/example/a11_pd2_pa_giruckis_nikitins_2026/FirstFragment.java
+++ app/src/main/java/com/example/a11_pd2_pa_giruckis_nikitins_2026/FirstFragment.java
@@ -9,11 +9,12 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.a11_pd2_pa_giruckis_nikitins_2026.databinding.FragmentFirstBinding;
+import com.example.a11_pd2_pa_giruckis_nikitins_2026.databinding.ContentMainBinding;
+
 
 public class FirstFragment extends Fragment {
 
-    private FragmentFirstBinding binding;
+    private ContentMainBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +22,7 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        binding = ContentMainBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
